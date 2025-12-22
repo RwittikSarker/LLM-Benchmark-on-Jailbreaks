2b60d681f15a012bc41268c08a37fa5ae741261c
RwittikSarker <rwittiksarker@gmail.com>
2025-12-23 01:32:47 +0600

    llama result

--- defense_hparams.py
+++ defense_hparams.py
@@ -8,16 +8,9 @@ def register_hparams(defense: type):
         hparams[name] = default_value
 
     if defense.__name__ == "SmoothLLM":
-        _hparam("number_of_copies", 2)
+        _hparam("number_of_copies", 1)
         _hparam("perturbation_type", "RandomSwapPerturbation")
-        _hparam("perturbation_pct", 10)
-
-    elif defense.__name__ == "PerplexityFilter":
-        _hparam("perplexity_model_path", "gpt2")
-
-    elif defense.__name__ == "EraseAndCheck":
-        _hparam("erase_length", 1)
-        _hparam("tokenizer_path", "gpt2")
+        _hparam("perturbation_pct", 5)
 
     else:
         raise ValueError(f"Defense {defense.__name__} not found in defense_hparams.py.")
